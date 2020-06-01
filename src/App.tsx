0c2dc65325fc30ff0dc1904cd5124228f969c792
shendan <664542051@qq.com>
2020-06-01 18:19:58 +0800

    定义文件结构和样式变量

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <hr/>
+        <code>
+          const a = b
+        </code>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
