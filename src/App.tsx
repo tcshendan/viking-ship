cc72c7f4b1da0e8cf6722a8cca673ee6a6064a31
shendan <664542051@qq.com>
2020-06-02 15:11:24 +0800

    fix: button组件开发

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <hr/>
-        <code>
-          const a = b
-        </code>
+        <Button className="custom"> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Disabled Link </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -24,7 +24,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
